97e307056daad67aa96283cb13c695b3f1d0060e
Gabriel Cardona <gabriel.earth.engineering@gmail.com>
2020-05-04 19:43:44 -0700

    Earth plugin

---
+++ earth.plugin.zsh
@@ -0,0 +1,6 @@
+function options() {
+    PLUGIN_PATH="$HOME/.oh-my-zsh/plugins/"
+    for plugin in $plugins; do
+        echo "\n\nPlugin: $plugin"; grep -r "^function \w*" $PLUGIN_PATH$plugin | awk '{print $2}' | sed 's/()//'| tr '\n' ', '; grep -r "^alias" $PLUGIN_PATH$plugin | awk '{print $2}' | sed 's/=.*//' |  tr '\n' ', '
+    done
+}
