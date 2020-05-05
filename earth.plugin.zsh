5d73ef7c973f3a365483da4237bf98da4dd093bf
Gabriel Cardona <gabriel.earth.engineering@gmail.com>
2020-05-04 20:25:05 -0700

    Formatting

--- earth.plugin.zsh
+++ earth.plugin.zsh
@@ -1,7 +1,6 @@
 function oh_my_zsh_plugins() {
     PLUGINS="$HOME/.oh-my-zsh/plugins/"
     for plugin in $plugins; do
-        echo "Plugin: $plugin"; grep -r "^function \w*" $PLUGINS$plugin | awk '{print $2}' | sed 's/()//'| tr '\n' ', '; grep -r "^alias" $PLUGINS$plugin | awk '{print $2}' | sed 's/=.*//' |  tr '\n' ', '
-        echo "\n-------"
+        echo "\n\nPlugin: $plugin"; grep -r "^function \w*" $PLUGINS$plugin | awk '{print $2}' | sed 's/()//'| tr '\n' ', '; grep -r "^alias" $PLUGINS$plugin | awk '{print $2}' | sed 's/=.*//' |  tr '\n' ', '
     done
 }
